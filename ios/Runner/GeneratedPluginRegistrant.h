ab31b4ad19cde2f3b1e632319150db931312e38e
OpenPlay Dev <dev@openplay.app>
2026-09-25 18:51:01 +0100

    fix(build): upgrade gradle to 8.14, fix Xcode PBXBuildFile reference, and automate release packaging

---
+++ ios/Runner/GeneratedPluginRegistrant.h
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#ifndef GENERATED_PLUGIN_REGISTRANT_
+#define GENERATED_PLUGIN_REGISTRANT_
+
+#import <Flutter/Flutter.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GeneratedPluginRegistrant : NSObject
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry;
+@end
+
+NS_ASSUME_NONNULL_END
+#endif /* GENERATED_PLUGIN_REGISTRANT_ */
